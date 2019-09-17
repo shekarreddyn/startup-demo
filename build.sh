83e601aaa029c707f234ed384843d1502cf28fe4
Shekar <shekaredu16@gmail.com>
2019-09-16 22:54:14 -0500

    init commit10

--- build.sh
+++ build.sh
@@ -2,11 +2,17 @@
 
 cd C:\Program Files (x86)\Jenkins\workspace\startup-demo-1
 
+sleep 2
+
 echo "run gradle clean"
+
+sleep 2
 
 $./gradlew clean
 
 echo "run gradle build"
+
+sleep 2
 
 $./gradlew build
 
