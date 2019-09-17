1ab8b33bff1678a775c0ebf5d1ea0af892c4677b
Shekar <shekaredu16@gmail.com>
2019-09-16 20:51:29 -0500

    init commit

---
+++ src/main/java/com/startup/startupdemo/DiagnosisServiceApplication.java
@@ -0,0 +1,15 @@
+
+package com.startup.startupdemo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DiagnosisServiceApplication {
+
+    @GetMapping("/getDiagnosis")
+    public String getDoctorService() {
+        return "Diagnosis microservice called...";
+    }
+}
