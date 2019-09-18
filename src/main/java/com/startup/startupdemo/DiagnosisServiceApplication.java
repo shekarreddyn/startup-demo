a9c6e23d5db7c8ba8ec2195add82c5c75f7193ce
Shekar <shekaredu16@gmail.com>
2019-09-17 21:26:54 -0500

    ci/cd

--- src/main/java/com/startup/startupdemo/DiagnosisServiceApplication.java
+++ src/main/java/com/startup/startupdemo/DiagnosisServiceApplication.java
@@ -10,6 +10,6 @@ public class DiagnosisServiceApplication {
 
     @GetMapping("/getDiagnosis")
     public String getDoctorService() {
-        return "Diagnosis microservice called. hello world..";
+        return "Diagnosis microservice called. hello shekar..";
     }
 }
